2fd0df07b12c6befcb15180daec04d2da0cdebe5
Kei Nishimura-Gasparian <kei.nishimuragasparian@gmail.com>
2024-11-11 17:27:31 -0500

    added requirements.txt

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from setuptools import setup, find_packages
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(
     name="transformers_102",
     version="0.1",
     package_dir={"transformers_102": "src"},
     packages=find_packages(),
+    install_requires=requirements,
 )
